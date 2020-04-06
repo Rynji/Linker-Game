8bb941fd393b3ae737b4f60cefcdeb78dbd3fe1d
Timon <timon@springlab.nl>
2020-04-06 16:18:25 +0200

    Grid creates itself and fills with tiles which get assigned a random id and sprite.

--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -4,15 +4,76 @@ using UnityEngine;
 
 public class GridController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject tilePrefab;
+    [SerializeField]
+    private int rows, cols = 0;
+    [SerializeField]
+    private float tileSize;
+    [Header("Debug Tools")]
+    [SerializeField]
+    private bool setGridUpdate;
+
+    private Tile[,] gridTiles;
+
+    public Tile[,] GridTiles
+    {
+        get
+        {
+            return gridTiles;
+        }
+
+        set
+        {
+            gridTiles = value;
+        }
+    }
+
+
     void Start()
     {
+        gridTiles = new Tile[rows, cols];
+
+        FillGrid();
+    }
+
+    void Update()
+    {
+        //Used at the beginning of development to determine grid size/positioning.
+        if (setGridUpdate)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    gridTiles[i, j].transform.position = GetGridPosition(i, j);
+                }
+            }
+        }
+    
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FillGrid()
     {
-        
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                GameObject go = Instantiate(tilePrefab, GetGridPosition(i, j), Quaternion.identity, this.gameObject.transform);
+                go.name = "Tile_" + i + "_" + j;
+                gridTiles[i, j] = go.GetComponent<Tile>();
+                gridTiles[i, j].SetVisual();
+            }
+        }
+    }
+
+    private Vector3 GetGridPosition(int i, int j)
+    {
+        Vector3 gridPos = new Vector3();
+
+        gridPos = this.gameObject.transform.localPosition + new Vector3((i * tileSize), (-j * tileSize), 0f);
+
+        return gridPos;
     }
 }
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [Header("Visuals")]
+    [SerializeField] private List<Sprite> visualSprites;
+
+    private int randomID;
+
+    public int RandomID { get => randomID; }
+
+    
+    public void SetVisual()
+    {
+        randomID = Random.Range(0, visualSprites.Count);
+        this.GetComponent<SpriteRenderer>().sprite = visualSprites[randomID];
+    }
+}
